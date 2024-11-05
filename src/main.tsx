b928b3000648b95e50bccf1c4f785f742a06df53
Michael L Petersen <ekmlpe@danskespil.dk>
2024-11-05 15:28:59 +0100

    add ds css and tailwind config

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import './index.css'
+import './styles/ds/Core.less'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -6,7 +6,7 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
     return (
-        <div className="p-2">
+        <div className="p-10">
             <h3>Welcome Home!</h3>
         </div>
     )
