ee255fd14fa7377a7056e2795b4f1b198d0d825e
Michael L Petersen <ekmlpe@danskespil.dk>
2024-11-07 12:01:47 +0100

    add cancel to kontaktinfo

--- src/routes/kontaktinformationer.tsx
+++ src/routes/kontaktinformationer.tsx
@@ -2,25 +2,44 @@ import {createFileRoute} from '@tanstack/react-router'
 import {TopBar} from "../Components/TopBar/TopBar.tsx";
 import {Container} from "../Components/Container/Container.tsx";
 import {Header} from "../Components/Header/Header.tsx";
+import {useState} from "react";
+import {CancelSignUp} from "../Components/CancelSignUp/CancelSignUp.tsx";
 
 export const Route = createFileRoute('/kontaktinformationer')({
     component: Kontaktinformationer,
 })
 
 function Kontaktinformationer() {
+    const [showCancel, setShowCancel] = useState(false);
+
+    const handleCancel = () => {
+        setShowCancel(!showCancel);
+    }
+
     return (
+
         <>
-            <TopBar currentStep={1} steps={7}/>
 
-            <Container>
+            {showCancel && (
+                <CancelSignUp handleCancel={handleCancel}/>
+            )}
 
-                {/* Max width 392px is only for developing, to make output look exactly like Figma. Should be removed. */}
-                <div className={'max-w-[360px]'}>
-                    <Header title={'Kontaktinformationer'}
-                            text={"Indtast venligst din e-mailadresse og telefonnummer. "}/>
-                </div>
+            {!showCancel && (
 
-            </Container>
+                <>
+                    <TopBar currentStep={1} steps={7} handleCancel={handleCancel}/>
+
+                    <Container>
+
+                        {/* Max width 392px is only for developing, to make output look exactly like Figma. Should be removed. */}
+                        <div className={'max-w-[360px]'}>
+                            <Header title={'Kontaktinformationer'}
+                                    text={"Indtast venligst din e-mailadresse og telefonnummer. "}/>
+                        </div>
+
+                    </Container>
+                </>
+            )}
         </>
     )
 }
